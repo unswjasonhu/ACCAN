02de61b4aae5a78505044859a6bf34e1bebd2c58
unswjasonhu <jasonhu.au@gmail.com>
2017-01-09 22:18:51 +1100

    Update models.py

--- sensors/models.py
+++ sensors/models.py
@@ -4,7 +4,12 @@ from django.db import models
 
 # Create your models here.
 class DeviceGeneral(models.Model):
-    DeviceID=models.IntegerField
+    DeviceID=models.IntegerField(max_length=30, primary_key=True)
+    SleepTime
+    ActiveVolume 
+    AvgPacketSize
+    Mean Rate 
+    
 
     
 
