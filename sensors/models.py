087a29102d189048eeff7781d48e41b48ee1c2b5
unswjasonhu <jasonhu.au@gmail.com>
2017-01-10 14:20:15 +1100

    Update models.py

--- sensors/models.py
+++ sensors/models.py
@@ -4,11 +4,24 @@ from django.db import models
 
 # Create your models here.
 class DeviceGeneral(models.Model):
-    DeviceID=models.IntegerField(max_length=30, primary_key=True)
-    SleepTime
-    ActiveVolume 
-    AvgPacketSize
-    Mean Rate 
+    DeviceID = models.IntegerField(max_length=30, primary_key=True)
+    SleepTime = models.IntegerField(max_length=30)
+    ActiveVolume = models.IntegerField(max_length=30)
+    AvgPacketSize = models.IntegerField(max_length=30)
+    MeanRate = models.IntegerField(max_length=30)
+    PeakRate = models.IntegerField(max_length=30)
+    ActiveTime = models.IntegerField(max_length=30)
+    NumServersComm = models.IntegerField(max_length=30)
+    NumProtocols = models.IntegerField(max_length=30)
+    UniqueDNSReq = models.IntegerField(max_length=30)
+    DNSInterval = models.IntegerField(max_length=30)
+    NTPInterval = models.IntegerField(max_length=30)
+    PortsOpen = models.TextField()
+    PortComm = models.TextField()
+    EncryptionProtocol = models.TextField()
+    
+    
+    
     
 
     
