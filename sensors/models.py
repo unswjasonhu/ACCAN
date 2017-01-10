26b09f5ce3ad2b1f58a937fc05de8245ebca7640
unswjasonhu <jasonhu.au@gmail.com>
2017-01-10 15:36:13 +1100

    Update models.py

--- sensors/models.py
+++ sensors/models.py
@@ -14,7 +14,7 @@ class DeviceGeneral(models.Model):
     PictureUrl = models.URLField()
     Availabilty = models.CharField(max_length=30)
     Price = models.IntegerField(max_length=30)
-    Availabilty models.CharField(max_length=30)
+    Availabilty = models.CharField(max_length=30)
     Price = models.IntegerField()
     Interactivity = models.TextField()
     AppName = models.TextField()
