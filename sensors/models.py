5fc9bbb315146ac8a9dd746edc63965ab5d8ef49
unswjasonhu <jasonhu.au@gmail.com>
2017-01-11 19:03:26 +1100

    Update models.py

--- sensors/models.py
+++ sensors/models.py
@@ -26,6 +26,8 @@ class DeviceGeneral(models.Model):
                                                                                     self.PictureUrl, self.Availabilty, self.Price, \
                                                                                     self.Interactivity, self.AppName, self.FirmVersion, \
                                                                                     self.DateTested, self.OS, self.ComunicationMethod)
+    class Meta:
+        ordering = ['DeviceID']
     
 class DeviceTrafficPattern(models.Model):
     DeviceID = models.IntegerField(primary_key=True)
@@ -48,7 +50,9 @@ class DeviceTrafficPattern(models.Model):
                                                                                     self.MeanRate, self.PeakRate, self.ActiveTime, \
                                                                                     self.NumServersComm, self.NumProtocols, self.UniqueDNSReq, \
                                                                                     self.DNSInterval, self.NTPInterval, self.PortsOpen, \
-                                                                                    self.PortComm, self.EncryptionProtocol) 
+                                                                                    self.PortComm, self.EncryptionProtocol)
+    class Meta:
+        ordering = ['DeviceID']
     
 class Exploit(models.Model):
     ExploitID = models.IntegerField(primary_key=True)
@@ -60,14 +64,18 @@ class Exploit(models.Model):
     Characteristic = models.TextField()
     def __str__(self):
         return u'%s, %s, %s, %s, %s, %s, %s' % (self.ExploitID, self.DeviceID, self.ExploitInfoID, self.ExploitDetail,\
-                                               self.Script, self.SamplePCAP, self. Characteristic) 
+                                               self.Script, self.SamplePCAP, self. Characteristic)
+    class Meta:
+        ordering = ['ExploitID']
     
 class ExploitInfo(models.Model):
     ExploitInfoID = models.IntegerField(primary_key=True)
     ExploitName = models.TextField()
     ExploitInfo = models.TextField()
     def __str__(self):
-        return u'%s, %s, %s' % (self.ExploitInfoID, self.ExploitName, self.ExploitInfo)     
+        return u'%s, %s, %s' % (self.ExploitInfoID, self.ExploitName, self.ExploitInfo)
+    class Meta:
+        ordering = ['ExploitInfoID']
     
     
     
