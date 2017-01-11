15e0f4f8fa1d504453de5ffd84ae40810315328c
unswjasonhu <jasonhu.au@gmail.com>
2017-01-12 01:14:06 +1100

    Update views.py

--- sensors/views.py
+++ sensors/views.py
@@ -1,17 +1,49 @@
 # Create your views here.
-
-from django.http import HttpResponse
+import json
 from django.shortcuts import render
+# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from sensors.models import DeviceGeneral
+from sensors.serializer import DeviceSerializer
 
-def search_form(request):
-        return render(request, 'search_form.html')
+@api_view(['GET','POST'])
+        def Sensor_list(request):
+                if request.method=="GET":
+                        sensors = DeviceGeneral.objects.all()
+                        serializer = DeviceSerializer(sensors,many=True)
+                        return Response(serializer.data)
 
-def search(request):
-    if 'q' in request.GET and request.GET['q']:
-        q = request.GET['q']
-        devicegeneral = DeviceGeneral.objects.filter(Name__icontains=q)
-        return render(request, 'search_results.html',\
-                {'devicegeneral': devicegeneral, 'query': q})
-    else:
-        return render(request, 'search_form.html', {'error': True})
+                elif request.method == 'POST':
+                        print request.body
+                        serializer = DeviceSerializer(data=json.loads(request.body))
+                        if serializer.is_valid():
+                                serializer.save()
+                                return Response(serializer.data, status=status.HTTP_201_CREATED)
+                        else:
+                                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE']) #
+        def Sensor_detail(request,pk):
+                try:
+                        sensors = DeviceGeneral.objects.get(pk=pk)
+                except DeviceGeneral.DoesNotExist:
+                        return Response(status=status.HTTP_404_NOT_FOUND)
+
+        if request.method == "GET":
+                serializer = DeviceGeneralSerializer(sensors)
+                return Response(serializer.data)
+
+        elif request.method == "PUT":
+                serializer = DeviceGeneralSerializer(sensors,data=json.loads(request.body))
+                if serializer.is_valid():
+                        serializer.save()
+                        return Response(serializer.data)
+                else:
+                        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+        elif request.method == "DELETE":
+                sensors.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
