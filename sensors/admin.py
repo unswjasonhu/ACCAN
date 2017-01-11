b53c52961f395ff4fac8df10a395c1b3f1954e9f
unswjasonhu <jasonhu.au@gmail.com>
2017-01-11 20:47:36 +1100

    Update admin.py

--- sensors/admin.py
+++ sensors/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import 
+from .models import DeviceGeneral, DeviceTrafficPattern, Exploit, ExploitInfo
 
-admin.site.register(Publisher)
-admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(DeviceGeneral)
+admin.site.register(DeviceTrafficPattern)
+admin.site.register(Exploit)
+admin.site.register(ExploitInfo)
